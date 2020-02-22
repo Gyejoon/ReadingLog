c7b727af99296ffb9604a09b59d9543f0195a48b
Gyejoon <00700mm@gmail.com>
2020-02-22 23:38:45 +0900

    feat: teal, gray, red색상표 추가

--- client/src/lib/styles/palette.ts
+++ client/src/lib/styles/palette.ts
@@ -1,4 +1,38 @@
 const palette = {
+  /* teal */
+  teal0: '#F3FFFB',
+  teal1: '#C3FAE8',
+  teal2: '#96F2D7',
+  teal3: '#63E6BE',
+  teal4: '#38D9A9',
+  teal5: '#20C997',
+  teal6: '#12B886',
+  teal7: '#0CA678',
+  teal8: '#099268',
+  teal9: '#087F5B',
+  /* gray */
+  gray0: '#F8F9FA',
+  gray1: '#F1F3F5',
+  gray2: '#E9ECEF',
+  gray3: '#DEE2E6',
+  gray4: '#CED4DA',
+  gray5: '#ADB5BD',
+  gray6: '#868E96',
+  gray7: '#495057',
+  gray8: '#343A40',
+  gray9: '#212529',
+  /* red */
+  red0: '#fff5f5',
+  red1: '#ffe3e3',
+  red2: '#ffc9c9',
+  red3: '#ffa8a8',
+  red4: '#ff8787',
+  red5: '#ff6b6b',
+  red6: '#fa5252',
+  red7: '#f03e3e',
+  red8: '#e03131',
+  red9: '#c92a2a',
+  /* blue map */
   blue: {
     bright: '#0267ff',
     soft: '#5e9eff',
