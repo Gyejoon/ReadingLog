d4abdaf8ab983b28f12d25abdefe3be661537c0f
Jun Young Kang <00700mm@gmail.com>
2020-03-29 00:05:17 +0900

    feat: 라우터 관련 세팅

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import { ApolloServer } from 'apollo-server-express';
 import schema from '@/graphql/schema';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use('', routes);
 
 const server = new ApolloServer({
   schema,
--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -3,6 +3,11 @@ import v1Router from './v1';
 
 const router = express.Router();
 
+router.get('/version', (_, res) => {
+  return res.json({
+    version: ['v1'],
+  });
+});
 router.use('/v1', v1Router);
 
 export default router;
