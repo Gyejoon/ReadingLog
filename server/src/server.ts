c7e56d6a4915c7b0c9e7a38354aa664e7a3023c9
Jun Young Kang <00700mm@gmail.com>
2020-03-28 23:36:56 +0900

    feat: 데이터베이스 관련 세팅

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
 import './env';
 import app from './app';
-// import Database from './database';
+import Database from './database';
 
 const { PORT } = process.env;
 
-// const database = new Database();
-// database.getConnection();
+const database = new Database();
+database.getConnection();
 
 app.listen(PORT, () => {
   console.log('ReadingLog server is listening to port', PORT);
