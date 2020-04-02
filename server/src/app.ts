74e7f44475f866eae998f7845be897446af38294
Jun Young Kang <00700mm@gmail.com>
2020-04-03 02:20:57 +0900

    refactor: Cors 설정

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,7 +10,13 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionsSuccessStatus: 200,
+  }),
+);
 app.use(authorization);
 app.use('', routes);
 
