c5333fa95d255ec9f307869def52bd0177b97d4a
Jun Young Kang <00700mm@gmail.com>
2020-04-14 00:54:05 +0900

    feat: 로그아웃 기능 구현

--- server/src/routes/api/v1/auth/index.ts
+++ server/src/routes/api/v1/auth/index.ts
@@ -1,9 +1,24 @@
-import express from 'express';
+import express, { Response } from 'express';
 import { localRegister, localLogin } from './auth.ctrl';
 
 const router = express.Router();
 
 router.post('/register/local', localRegister);
 router.post('/login/local', localLogin);
+router.post('/logout', (_, res: Response) => {
+  res.cookie('access_token', '', {
+    domain:
+      process.env.NODE_ENV === 'development'
+        ? undefined
+        : '.reading-log.coo.kr',
+  });
+
+  res.cookie('refresh_token', '', {
+    domain:
+      process.env.NODE_ENV === 'development'
+        ? undefined
+        : '.reading-log.coo.kr',
+  });
+});
 
 export default router;
