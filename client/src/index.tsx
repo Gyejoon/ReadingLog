3f36ea096b6b6c92b1349ced1850ecfbe1c34eca
Jun Young Kang <00700mm@gmail.com>
2020-04-14 00:54:23 +0900

    chore: Facebook SDK 셋팅

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -28,6 +28,29 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
+(window as any).fbAsyncInit = function() {
+  (window as any).FB.init({
+    appId: '215662702997532',
+    cookie: true,
+    xfbml: true,
+    version: 'v6.0',
+  });
+
+  (window as any).FB.AppEvents.logPageView();
+};
+
+(function(d, s, id) {
+  var js,
+    fjs = d.getElementsByTagName(s)[0] as any;
+  if (d.getElementById(id)) {
+    return;
+  }
+  js = d.createElement(s) as any;
+  js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk.js';
+  fjs.parentNode.insertBefore(js, fjs);
+})(document, 'script', 'facebook-jssdk');
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
