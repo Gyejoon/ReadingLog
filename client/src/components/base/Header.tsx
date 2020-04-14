61ddb3711621d6c25b7246d34ef0b74af086dcd7
Jun Young Kang <00700mm@gmail.com>
2020-04-14 17:17:21 +0900

    feat: 로그인 이후 헤더에 사용자 정보 표시하는 기능 구현

--- client/src/components/base/Header.tsx
+++ client/src/components/base/Header.tsx
@@ -4,6 +4,44 @@ import palette from 'lib/styles/palette';
 import media from 'lib/styles/media';
 import Navigation from './Navigation';
 import { Link } from 'react-router-dom';
+import useHeader from './hooks/useHeader';
+import { userThumbnail } from 'static/images';
+import { MdArrowDropDown } from 'react-icons/md';
+
+interface HeaderProps {}
+
+const Header: React.SFC<HeaderProps> = () => {
+  const { user, path, showLoginModal, showRegisterModal } = useHeader();
+
+  return (
+    <HeaderBlock>
+      <div className="title">
+        <Link to="/">
+          <span>ReadingLog</span>
+        </Link>
+      </div>
+      <Navigation path={path} />
+      {user ? (
+        <HeaderRight>
+          <img src={user.profile.thumbnail || userThumbnail} alt="thumbnail" />
+          <div className="dropdown-wrapper">
+            <span>{user.profile.nickname}</span>
+            <MdArrowDropDown className="icons" size="24"></MdArrowDropDown>
+          </div>
+        </HeaderRight>
+      ) : (
+        <HeaderRight>
+          <button className="sign-in" onClick={showLoginModal}>
+            로그인
+          </button>
+          <button className="sign-up" onClick={showRegisterModal}>
+            회원가입
+          </button>
+        </HeaderRight>
+      )}
+    </HeaderBlock>
+  );
+};
 
 const HeaderBlock = styled.div`
   display: flex;
@@ -30,68 +68,60 @@ const HeaderBlock = styled.div`
       padding-left: 8px;
     }
   }
+`;
 
-  .right {
-    padding-right: 24px;
+const HeaderRight = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-right: 24px;
 
-    button {
-      display: inline-flex;
-      align-items: center;
-      justify-content: center;
-      font-weight: bold;
-      cursor: pointer;
-      outline: none;
-      border: none;
-      color: white;
-      font-size: 15px;
-      width: 85px;
-      height: 36.5px;
+  img {
+    display: block;
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    margin-right: 12px;
+    object-fit: cover;
+    transition: 0.125s all ease-in;
+  }
+
+  .dropdown-wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    .icons {
+      margin-left: 4px;
     }
+  }
 
-    .sign-in {
-      color: #4378ff;
-      background-color: #ffffff;
-    }
+  button {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    font-weight: bold;
+    cursor: pointer;
+    outline: none;
+    border: none;
+    color: white;
+    font-size: 15px;
+    width: 85px;
+    height: 36.5px;
+  }
 
-    .sign-up {
-      color: #ffffff;
-      background-color: #4378ff;
-      ${media.small} {
-        display: none;
-      }
+  .sign-in {
+    color: #4378ff;
+    background-color: #ffffff;
+  }
+
+  .sign-up {
+    color: #ffffff;
+    background-color: #4378ff;
+    ${media.small} {
+      display: none;
     }
   }
 `;
-
-interface HeaderProps {
-  path: string;
-  showLoginModal: () => void;
-  showRegisterModal: () => void;
-}
-
-const Header: React.SFC<HeaderProps> = ({
-  path,
-  showLoginModal,
-  showRegisterModal,
-}) => {
-  return (
-    <HeaderBlock>
-      <div className="title">
-        <Link to="/">
-          <span>ReadingLog</span>
-        </Link>
-      </div>
-      <Navigation path={path} />
-      <div className="right">
-        <button className="sign-in" onClick={showLoginModal}>
-          로그인
-        </button>
-        <button className="sign-up" onClick={showRegisterModal}>
-          회원가입
-        </button>
-      </div>
-    </HeaderBlock>
-  );
-};
 
 export default Header;
