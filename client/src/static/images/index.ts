5090620799b843a944661713857269033e71e90f
Jun Young Kang <00700mm@gmail.com>
2020-04-16 02:56:29 +0900

    etc: 사용자 페이지 배너 이미지

--- client/src/static/images/index.ts
+++ client/src/static/images/index.ts
@@ -1,2 +1,3 @@
 export { default as mainBanner } from './main-banner.png';
 export { default as userThumbnail } from './user-thumbnail.png';
+export { default as userPageBanner } from './user-page-banner.png';
