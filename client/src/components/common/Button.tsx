9c28f8b3191aa4d67b500e144c27797a64d8555b
Gyejoong <00700mm@gmail.com>
2020-07-20 02:34:04 +0900

    feat: 공통 버튼 컴포넌트

--- client/src/components/common/Button.tsx
+++ client/src/components/common/Button.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const ButtonBlock = styled.button``;
+interface Props {}
 
-interface ButtonProps {}
-
-const Button: React.SFC<ButtonProps> = () => {
-  return <ButtonBlock></ButtonBlock>;
+const Button: React.SFC<Props> = ({ children }) => {
+  return <Block>{children}</Block>;
 };
+
+const Block = styled.button`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 2px;
+  width: 76px;
+  height: 30px;
+  border: 1px solid #0085ff;
+  background-color: #ffffff;
+  font-size: 12px;
+  color: #0085ff;
+  cursor: pointer;
+  outline: none;
+`;
 
 export default Button;
