c2eee69bbd0abd82bba8d8ae1daaae614c96d925
Jun Young Kang <00700mm@gmail.com>
2020-07-20 02:36:37 +0900

    refactor: UserIntro Container 구성

---
+++ client/src/containers/user/UserIntroContainer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import UserIntro from 'components/user/UserIntro';
+
+interface Props {}
+
+const UserIntroContainer: React.FC<Props> = () => {
+  return <UserIntro />;
+};
+
+export default UserIntroContainer;
